7312cc425ab94b4c1e85a9b5bfa417935cc0836f
Gek Siong Low <geksiong@yahoo.com>
2023-12-09 23:12:24 +0800

    style: configure Prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,11 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  printWidth: 100,
+	semi: true,
+	singleQuote: false,
+  useTabs: false,
+  tabWidth: 2,
   plugins: [
     'prettier-plugin-astro',
     'prettier-plugin-tailwindcss'
