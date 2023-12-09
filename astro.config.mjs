e8639192ac844fba8e5caeb737c7d84bbbf8d178
Gek Siong Low <geksiong@yahoo.com>
2023-12-09 22:10:46 +0800

    feat: Add Astro Sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  site: 'https://change.me',
+  integrations: [mdx(), tailwind(), sitemap()]
 });
