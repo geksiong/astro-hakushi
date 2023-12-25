ddf32cef0ee43149c2138c59945f037e2e63ead2
Gek Siong Low <geksiong@yahoo.com>
2023-12-25 20:54:55 +0800

    feat: robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
-
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://change.me",
-  integrations: [mdx(), tailwind(), sitemap()],
+  integrations: [mdx(), tailwind(), sitemap(), robotsTxt()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     rehypePlugins: [
@@ -19,8 +19,17 @@ export default defineConfig({
         rehypeExternalLinks,
         {
           content: [
-            { type: "text", value: " " },
-            { type: "element", tagName: "i", properties: { class: "external-link" } },
+            {
+              type: "text",
+              value: " ",
+            },
+            {
+              type: "element",
+              tagName: "i",
+              properties: {
+                class: "external-link",
+              },
+            },
           ],
         },
       ],
