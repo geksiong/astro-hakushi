16e0e9693794a5af37e01267dc4ce11bdbd429ad
Gek Siong Low <geksiong@yahoo.com>
2023-12-25 19:50:06 +0800

    feat(post): reading time, word count, external links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,32 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
 
+import rehypeExternalLinks from "rehype-external-links";
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://change.me',
+  site: "https://change.me",
   integrations: [mdx(), tailwind(), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          content: [
+            { type: "text", value: " " },
+            { type: "element", tagName: "i", properties: { class: "external-link" } },
+          ],
+        },
+      ],
+    ],
+  },
   vite: {
-    plugins: [yaml()]
-  }
+    plugins: [yaml()],
+  },
 });
