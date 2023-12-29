9bd07ab360fbe885f41c5e87f0e97ed5a3c56fec
Gek Siong Low <geksiong@yahoo.com>
2023-12-29 23:01:28 +0800

    style: initial theme colors

--- src/content/demo/markdown-remark-plugins.md
+++ src/content/demo/markdown-remark-plugins.md
@@ -139,6 +139,8 @@ These callouts are provided by `@microflash/remark-callout-directives` and uses 
 
 :::note
 Some **content** with _Markdown_ `syntax`.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 :::
 
 :::commend
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,12 +3,20 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["Atkinson Hyperlegible", ...defaultTheme.fontFamily.sans],
         heading: ["Signika Negative", ...defaultTheme.fontFamily.sans],
         mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        text: "var(--text)",
+        background: "var(--background)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        accent: "var(--accent)",
       },
     },
   },
