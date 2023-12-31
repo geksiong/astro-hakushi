fc7c5a9d1435a4464d248901397b9edf890c97ad
Gek Siong Low <geksiong@yahoo.com>
2023-12-31 14:08:57 +0800

    feat: add alpine.js integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,23 +2,19 @@ import { defineConfig } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
-
 import robotsTxt from "astro-robots-txt";
 import robotsConfig from "./robots-txt.config";
-
 import webmanifest from "astro-webmanifest";
 import webmanifestConfig from "./webmanifest.config";
-
 import remarkEmoji from "remark-emoji";
-
 import expressiveCode from "astro-expressive-code";
 import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-sections";
-
 import remarkDirective from "remark-directive";
 import remarkCalloutDirectives from "@microflash/remark-callout-directives";
 
@@ -26,12 +22,15 @@ import remarkCalloutDirectives from "@microflash/remark-callout-directives";
 export default defineConfig({
   site: "https://change.me",
   integrations: [
-    expressiveCode({ plugins: [pluginCollapsibleSections()] }),
+    expressiveCode({
+      plugins: [pluginCollapsibleSections()],
+    }),
     mdx(),
     tailwind(),
     sitemap(),
     robotsTxt(robotsConfig),
     webmanifest(webmanifestConfig),
+    alpinejs(),
   ],
   markdown: {
     remarkPlugins: [
@@ -39,7 +38,13 @@ export default defineConfig({
       remarkMath,
       remarkDirective,
       remarkCalloutDirectives,
-      [remarkEmoji, { accessible: true, emoticon: true }],
+      [
+        remarkEmoji,
+        {
+          accessible: true,
+          emoticon: true,
+        },
+      ],
     ],
     rehypePlugins: [
       [rehypeKatex, {}],
@@ -59,7 +64,9 @@ export default defineConfig({
               },
             },
           ],
-          properties: { className: ["external-link"] },
+          properties: {
+            className: ["external-link"],
+          },
         },
       ],
     ],
